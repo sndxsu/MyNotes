01669a5f0bd29c7dba27f03cd31d64491f260154
sndxsu <sndx.su@gmail.com>
2024-11-07 10:40:41 +0200

    Can show description and added button

--- app/src/main/java/com/sndx/mynotes/screens/components/ListItem.kt
+++ app/src/main/java/com/sndx/mynotes/screens/components/ListItem.kt
@@ -2,11 +2,15 @@ package com.sndx.mynotes.screens.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,20 +23,25 @@ fun ListItem(name: String, prof:String){
     Card (
         Modifier
             .fillMaxWidth()
-            .height(150.dp)
             .padding(10.dp), RoundedCornerShape(10.dp)
     ){
-        Row (Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
-//            Image(painter = painterResource(id = R.drawable.anime_girl), contentDescription = "r",
-//                Modifier
-//                    .fillMaxHeight()
-//                    .padding(5.dp)
-//                    .clip(
-//                        CircleShape
-//                    ))
-            Column (Modifier.padding(5.dp)){
+        Row (Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically){
+            Column (horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .padding(start = 10.dp, top = 5.dp, end = 5.dp)
+                ){
                 Text(text = name)
                 Text(text = prof)
+            }
+
+            IconButton(
+                onClick = { /*todo*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = "Important"
+                )
             }
         }
     }
